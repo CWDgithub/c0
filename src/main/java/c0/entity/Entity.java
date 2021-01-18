25c54b2f8db7f669df69bf658b5f276e05719188
cwd <cwd@localhost.localdomain>
2021-01-18 20:35:35 +0800

    update 9

--- src/main/java/c0/entity/Entity.java
+++ src/main/java/c0/entity/Entity.java
@@ -14,4 +14,27 @@ public abstract class Entity extends AbstractNode {
     Entity(String name) {
         this.name = name;
     }
+
+    public static byte[] longToByte(long val) {
+        byte[] b = new byte[8];
+        b[7] = (byte) (val & 0xff);
+        b[6] = (byte) ((val >> 8) & 0xff);
+        b[5] = (byte) ((val >> 16) & 0xff);
+        b[4] = (byte) ((val >> 24) & 0xff);
+        b[3] = (byte) ((val >> 32) & 0xff);
+        b[2] = (byte) ((val >> 40) & 0xff);
+        b[1] = (byte) ((val >> 48) & 0xff);
+        b[0] = (byte) ((val >> 56) & 0xff);
+        return b;
+    }
+
+    public static byte[] intToByte(int val) {
+        byte[] b = new byte[4];
+        b[3] = (byte) (val & 0xff);
+        b[2] = (byte) ((val >> 8) & 0xff);
+        b[1] = (byte) ((val >> 16) & 0xff);
+        b[0] = (byte) ((val >> 24) & 0xff);
+        return b;
+    }
+
 }
