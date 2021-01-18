2015913bdf8f435408b31c582cf1280daac7d28d
cwd <cwd@localhost.localdomain>
2021-01-18 22:23:36 +0800

    update 13

---
+++ src/main/java/c0/entity/enty/tableStruct.java
@@ -0,0 +1,13 @@
+package c0.entity.enty;
+
+/**名字表的定义
+ *
+ */
+public class tableStruct {
+    public String name;
+    public Constant.nameTable type;
+    public int value;
+    public int level;
+    public int addr;
+    public int size;
+}
