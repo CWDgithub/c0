a7ee7eb9e8bcb7754174570bbf4f52dfb70d63ee
cwd <cwd@localhost.localdomain>
2021-01-18 19:19:56 +0800

    update 6

---
+++ src/main/java/c0/JavaCC/SymbolItem.java
@@ -0,0 +1,89 @@
+package c0.JavaCC;// package com.imudges.C0Compiler.JavaCC;
+/**
+ * @author shianqi@imudges.com
+ * Created by shianqi on 2016/12/13
+ */
+public class SymbolItem {
+    public SymbolItem(SymbolType type) {
+        this.type = type;
+        this.name = "";
+        this.val = 0;
+        this.level = 0;
+        this.adr = 0;
+        this.size = 0;
+        this.returnType = 0;
+    }
+
+    public String toString(){
+        return type.toString()+"\t"+name+"\t"+adr+"\t"+returnType;
+    }
+
+    public enum SymbolType{
+        intSym,
+        functionSym
+    }
+
+    private String name;
+    private SymbolType type;
+    private int val;
+    private int level;
+    private int adr;
+    private int size;
+    private int returnType;
+
+    public int getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(int returnType) {
+        this.returnType = returnType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SymbolType getType() {
+        return type;
+    }
+
+    public void setType(SymbolType type) {
+        this.type = type;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getAdr() {
+        return adr;
+    }
+
+    public void setAdr(int adr) {
+        this.adr = adr;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/c0/type/Type.java
+++ src/main/java/c0/type/Type.java
@@ -1,5 +1,12 @@
 package c0.type;
 
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Type {
     TypeVal type;
 
@@ -40,4 +47,67 @@ public class Type {
         return stringBuilder;
     }
 
+    /**
+     * 简单的程序介绍
+     */
+    private void outputInformation(){
+        System.out.println("欢迎使用C0编译器");
+        System.out.println("本编译器实现功能：");
+    }
+
+    /**
+     * 初始化程序
+     *
+     */
+    private void init(){
+        outputInformation();
+        ArrayList<String> arrayList= new ArrayList<String>();
+        initFile();
+    }
+
+    /**
+     * 初始化文件
+     */
+    private void initFile(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("input the c0 file name");
+            if(readC0File(scanner.next())){
+                break;
+            }else{
+                System.out.println("c0 File read failed. Please try again!");
+            }
+        }
+    }
+
+    /**
+     * 读取文件
+     * @param fileName 文件名称
+     * @return 读取成功返回true，否则返回false
+     */
+    private boolean readC0File(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        ArrayList<String> arrayList= new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString;
+            while ((tempString = reader.readLine()) != null) {
+                arrayList.add(tempString);
+            }
+            reader.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    System.out.println("document close failed!");
+                }
+            }
+        }
+    }
 }
